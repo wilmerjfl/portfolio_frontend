b3c9f50f2c8c549e2e8718377ca98378a56ea751
Wilmer Jesus <wilmerjfl3@gmail.com>
2023-04-25 09:11:01 -0300

    update information and profile picture

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,27 +2,30 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Wilmer Jesus', // e.g: 'Name | Developer'
+  lang: 'es', // e.g: en, es, fr, jp
+  description: 'Software Engineer', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: `Hi there, I'm`,
-  name: 'Wilmer Fajardo',
-  subtitle: 'Internal Systems Analyst',
+  name: 'Wilmer Jesus',
+  subtitle: 'Software Engineer',
   cta: 'About Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: `I'm a IT Student`,
-  paragraphTwo: `With knowledge on web development with JavaScript
-  and Python Frameworks for frontend and backend, leading and planning
-  with agile for project development`,
-  paragraphThree: 'Now i still learning about new technologies and good practices.',
+  paragraphOne: `I'm Wil`,
+  paragraphTwo: 'A skilled Ssr software engineer with a strong background in Java and NodeJS.'
+          + ' I have a passion for coding and constantly strive to improve my skills,'
+          + ' staying up to date with the latest technologies and development methodologies.'
+          + ' One of my favorite things to do is to mentor and teach other developers.'
+          + ' I find it extremely rewarding to share my knowledge and experience,'
+          + ' helping others improve their coding skills and become better engineers.',
+  paragraphThree: '',
   resume: 'https://www.linkedin.com/in/wilmerjfl', // if no resume, the button will not show up
 };
 
@@ -36,6 +39,33 @@ export const projectsData = [
     info2: 'From Udemy NodeJS Api Masteclass Course.',
     url: '',
     repo: 'https://github.com/wilmerjfl/bootcamp_nodejs.git', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Meli Fresh Api',
+    info: 'This is a personal project build like a challenge to end a Java bootcamp.',
+    info2: 'From Digital House & Mercado Libre',
+    url: '',
+    repo: 'https://github.com/wilmerjfl/meli_fresh/tree/feature/us06', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Random data generator',
+    info: 'This is a python script to get random data to use like data in.',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/wilmerjfl/random_data', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Appointment System',
+    info: 'This is an appointment system build in react using localstorage.',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/wilmerjfl/appointmentSystem', // if no repo, the button will not show up
   },
 ];
 
